70b77dcc897468789a6b03205da4ec7b75744def
Reboot Student <cristiancabrera.1985@gmail.com>
2024-09-30 10:39:24 +0100

    constructor method

---
+++ player.js
@@ -0,0 +1,17 @@
+console.log("Clase player load!!");
+
+class Player {
+    constructor(x, y){
+        this.width = 50;
+        this.height = 50;
+        this.speed = 10;
+        this.ejeX = x;
+        this.ejeY = y;
+        this.directionY = 0;
+        this.lives = 3;
+    }
+
+    
+
+
+}
