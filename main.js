9c729becf8b327d754d3d5e1bfa8e75d732ad022
Reboot Student <cristiancabrera.1985@gmail.com>
2024-09-30 11:06:02 +0100

    Player position

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("Script load!!");
+
+let playField = document.getElementById("playField")
+let playerCharacter = new Player(20, 20);
+
+playerCharacter.insert();
--- player.js
+++ player.js
@@ -1,17 +1,24 @@
 console.log("Clase player load!!");
 
 class Player {
-    constructor(x, y){
-        this.width = 50;
-        this.height = 50;
-        this.speed = 10;
-        this.ejeX = x;
-        this.ejeY = y;
-        this.directionY = 0;
-        this.lives = 3;
-    }
+  constructor(x, y) {
+    this.width = 50;
+    this.height = 50;
+    this.speed = 10;
+    this.ejeX = x;
+    this.ejeY = y;
+    this.directionY = 0;
+    this.lives = 3;
+    this.sprite = document.createElement("div");
+  }
 
-    
-
-
+  insert() {
+    this.sprite.setAttribute("id", "player");
+    this.sprite.style.width = this.width + "px";
+    this.sprite.style.height = this.height + "px";
+    this.sprite.style.top = this.ejeY + "px";
+    this.sprite.style.left = this.ejeX + "px"; 
+    this.sprite.style.position = "absolute";
+    playField.appendChild(this.sprite);
+  }
 }
